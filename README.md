51dec1aa499804670abc000dfa51787da0cb2402
Dušan Simić <dusan.simic1810@gmail.com>
2019-01-24 01:19:24 +0100

    Renamed to mtg-gatherer-search and added readme
    Using search instead of scraper. Also added readme and made it a real
    module, not an express testing env.

---
+++ README.md
@@ -0,0 +1,63 @@
+# MTG Gatherer Search
+> A simple search for getting card info form search on [Gatherer](http://gatherer.wizards.com).
+
+## What is this?
+
+MTG Gatherer Search is a simple search module that I use in my card database project. It scrapes search results from [Magic: The Gathering Gatherer](http://gatherer.wizards.com) portal for searching cards. It returns a json that contains essential card info: name, mana cost, image url and multiverse id. It's written in JavaSciprt as a module that can be used in Node.js or web frameworks.
+
+## Usage
+
+You can install mtg-gatherer-search via `npm` or `yarn` or any other Node.js package manager that connects to npm repository.
+
+### Install
+``` bash
+$ npm i -P mtg-gatherer-search
+```
+or with Yarn
+``` bash
+$ yarn add mtg-gatherer-search
+```
+
+### Import
+
+Once module is installed just import it into your JS file.
+``` javascript
+const mtgGathererSearch = require('mtg-gatherer-search');
+
+// Or in ES6
+
+import * as mtgGathererSearch from 'mtg-gatherer-search';
+```
+
+### Scrape
+
+Now just call the function and get your search results in a json.
+``` javascript
+const result = await mtgGathererSearch('black');
+
+/*
+[
+	{
+		title: 'Black Lotus',
+		mana: {
+			Any: 0,
+			Red: 0,
+			White: 0,
+			Green: 0,
+			Blue: 0,
+			Black: 0,
+			Artifact: 0,
+			Total: 0
+		},
+		imageUrl: 'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=382866&type=card',
+		multiverseId: 382866
+	},
+	...
+]
+
+*/
+```
+
+## License
+
+MIT © [Dušan Simić](http://dusansimic.me)
--- index.js
+++ index.js
@@ -1,94 +1,76 @@
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const got = require('got');
 const $ = require('cheerio');
 
-const app = express();
-const server = http.createServer(app);
+/**
+ * MTG Gatherer Scraper
+ * @param {String} cardName Name of the card you search for
+ */
+const mgs = async cardName => {
+	// If card name is not specified, scream
+	const hasCardName = Boolean(cardName);
+	if (!hasCardName) {
+		throw new Error('Card name not specified!');
+	}
 
-app.use(bodyParser.json());
-app.use(cors());
+	// Get Magic Gatherer search page html
+	const response = await got.get(`http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[${cardName}]`);
+	const document = response.body;
 
-app.get('/:cardName', async (req, res) => {
-	try {
-		// Get card name
-		const {cardName} = req.params;
-		// If card name is not specified, scream
-		const hasCardName = Boolean(cardName);
-		if (!hasCardName) {
-			return res.status(400).send('Card name not specified!');
+	// Query table from html
+	const table = $('.cardItemTable tbody tr td table tbody .cardItem.evenItem', document);
+	// Get middle and left col from table
+	const middleCol = $('.middleCol .cardInfo', table)
+	const leftCol = $('.leftCol', table);
+
+	// Get titles of cards
+	const titleDocument = $('.cardTitle a', middleCol);
+
+	const data = [];
+	for (let i = 0; i < table.length; i++) {
+		// Get mana html for current card
+		const manaDocument = $('.manaCost img', middleCol[i]);
+
+		// Parse that mana html
+		const manaList = [];
+		for (let j = 0; j < manaDocument.length; j++) {
+			manaList.push(manaDocument[j].attribs.alt);
+		}
+		const manaTypes = ['Any', 'Red', 'White', 'Green', 'Blue', 'Black', 'Artifact', 'Total'];
+		const mana = {};
+		for (const type of manaTypes) {
+			mana[type] = manaList.reduce((acc, val) => {
+				// Count total number of mana
+				if (type === 'Total') {
+					return !isNaN(val) ? acc + parseInt(val, 10) : acc + 1;
+				} else if (type === 'Any' && !isNaN(val)) {
+					// Count colorless
+					return parseInt(val, 10);
+				} else if (val === type) {
+					// Count color
+					return acc + 1;
+				}
+				return acc;
+			}, 0);
 		}
 
-		// Get Magic Gatherer search page html
-		const response = await got.get(`http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[${cardName}]`);
-		const document = response.body;
+		// Get the image url
+		const imageUrlRelative = $('a img', leftCol[i])[0].attribs.src;
+		const imageUrl = `http://gatherer.wizards.com/${imageUrlRelative.slice(6)}`;
 
-		// Query table from html
-		const table = $('.cardItemTable tbody tr td table tbody .cardItem.evenItem', document);
-		// Get middle and left col from table
-		const middleCol = $('.middleCol .cardInfo', table)
-		const leftCol = $('.leftCol', table);
+		// Get id out of image url
+		const multiverseId = parseInt(imageUrlRelative.slice(imageUrlRelative.indexOf('multiverseid=') + 13, -10), 10);
 
-		// Get titles of cards
-		const titleDocument = $('.cardTitle a', middleCol);
-
-		const data = [];
-		for (let i = 0; i < table.length; i++) {
-			// Get mana html for current card
-			const manaDocument = $('.manaCost img', middleCol[i]);
-
-			// Parse that mana html
-			const manaList = [];
-			for (let j = 0; j < manaDocument.length; j++) {
-				manaList.push(manaDocument[j].attribs.alt);
-			}
-			const manaTypes = ['Any', 'Red', 'White', 'Green', 'Blue', 'Black', 'Artifact', 'Total'];
-			const mana = {};
-			for (const type of manaTypes) {
-				mana[type] = manaList.reduce((acc, val) => {
-					// Count total number of mana
-					if (type === 'Total') {
-						return !isNaN(val) ? acc + parseInt(val, 10) : acc + 1;
-					} else if (type === 'Any' && !isNaN(val)) {
-						// Count colorless
-						return parseInt(val, 10);
-					} else if (val === type) {
-						// Count color
-						return acc + 1;
-					}
-					return acc;
-				}, 0);
-			}
-
-			// Get the image url
-			const imageUrlRelative = $('a img', leftCol[i])[0].attribs.src;
-			const imageUrl = `http://gatherer.wizards.com/${imageUrlRelative.slice(6)}`;
-
-			// Get id out of image url
-			const multiverseId = parseInt(imageUrlRelative.slice(imageUrlRelative.indexOf('multiverseid=') + 13, -10), 10);
-
-			// Create an object
-			const object = {
-				title: titleDocument[i].children[0].data,
-				mana,
-				imageUrl,
-				multiverseId
-			};
-			data.push(object);
-		}
-
-		res.send(data);
-	} catch (error) {
-		res.status(500).send(new Error(error));
+		// Create an object
+		const object = {
+			title: titleDocument[i].children[0].data,
+			mana,
+			imageUrl,
+			multiverseId
+		};
+		data.push(object);
 	}
-});
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', error => {
-	if (error) {
-		throw error;
-	}
-	console.log(`Listening on ${PORT}`);
-})
+	return data;
+};
+
+module.exports = mgs;
